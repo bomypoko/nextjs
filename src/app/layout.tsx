3939b86544888bc707f37db102c1a23f35eb3531
Ratchasri Kidking <orc_hero@hotmail.com>
2024-06-10 20:05:06 +0700

    Push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            
           </ThemeProvider>
       </body>
     </html>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,15 +5,16 @@ const Hero = () => {
   return (
 
     <div className='pb-20 pt-36'>
-      {/* Spotlight Here */}
 
+      {/* Spotlight Here */}
         <div>
             <Spotlight className='-left-10 -top-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
             <Spotlight className='top-10 left-full h-[80vh] w-[50vw]'fill='blue'/>
             <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]'fill='blue'/>
         </div>
 
-        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] absolute flex items-center justify-center  top-0 left-0">
+      {/* Grid Zone  */}
+        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] absolute flex items-center justify-center top-0 left-0">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>
 
