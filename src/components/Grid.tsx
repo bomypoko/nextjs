2be922c56c2e9e22a473e7dd8cf30a87cc7371a9
Ratchasri Kidking <orc_hero@hotmail.com>
2024-06-14 13:58:29 +0700

    Add Grid items

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    
+    <section id="about">
+
+      <BentoGrid>
+        {[{title:'title1',description:'Disc1',id: 1},
+          {title:'title1',description:'Disc1',id: 1},
+          {title:'title1',description:'Disc1',id: 1},
+        ].map((item,index) => (
+          <BentoGridItem
+            id={item.id}
+            description={item.description}
+            title={item.title}
+            key={item.id}
+            />
+          
+        ))}
+      </BentoGrid>
+    
+    </section>
+    
   )
 }
 
